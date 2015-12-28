016ba247f38ba82703719a8aa867902c9bc1f228
lannng <lannguyen309@gmail.com>
2015-12-28 02:15:55 -0800

    removed handlebar.js, minor CSS changes

--- app/components/AddTeam.js
+++ app/components/AddTeam.js
@@ -7,7 +7,7 @@ class AddTeam extends React.Component {
 	render() {
 		console.log('rendering form page');
 		return (
-			<div className='text-center team-options'>
+			<div className='text-center team-options red-bg'>
 				<h2>Start A Team</h2>
 				<div className='col-sm-6'>
 					<button className='btn btn-large blue-btn'><Link to='/new_team'>Team on a school campus</Link></button>
