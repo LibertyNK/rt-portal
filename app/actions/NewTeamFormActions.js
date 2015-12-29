c09db345beb131e28ec565430133fe970e15e9f7
lannng <lannguyen309@gmail.com>
2015-12-29 13:41:52 -0800

    added NewTeamFormm component, actions and store

---
+++ app/actions/NewTeamFormActions.js
@@ -0,0 +1,14 @@
+import alt from '../alt';
+
+class NewTeamFormActions {
+	constructor() {
+		'requestTeamSuccess',
+		'requestTeamFail'
+	}
+
+	requestTeam(name, about, amount, location) {
+
+	}
+}
+
+export default alt.createActions(NewTeamFormActions);
--- app/components/NewTeamForm.js
+++ app/components/NewTeamForm.js
@@ -1,11 +1,60 @@
 import React from 'react';
+import NewTeamFormActions from '../actions/NewTeamFormActions';
+import NewTeamFormStore from '../stores/NewTeamFormStore';
 
 class NewTeamForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = NewTeamFormStore.getState();
+		this.onChange = this.onChange.bind(this);
+	}
+
+	componentDidMount() {
+		NewTeamFormStore.listen(this.onChange);
+	}
+
+	componentWillUnmount(){
+		NewTeamFormStore.unlisten(this.onChange);
+	}
+
+	onChange(state) {
+		this.setState(state);
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+
+	}
+
 	render() {
 
 		return (
-			<div className='alert alert-success text-center'>
-				<p>New Team Form page</p>
+			<div className='container new-team-form'>
+				<div className='row'>
+					<h3 className='text-center'>Start a team on your campus!</h3>
+					<form>
+						<div className='form-group'>
+							<label className='control-label'>What would you like your team name to be?</label>
+							<input type='text' className='form-control' ref="name" placeholder="Enter team name" />
+						</div>
+						<div className='form-group'>
+							<label className='control-label'>Where is your team located?</label>
+							<input type='text' className='form-control' ref="street-address" placeholder='Street Address' />
+							<input type='text' className='form-control' ref="city" placeholder='City' />
+							<input type='text' className='form-control' ref="state" placeholder='State' />
+							<input type='text' className='form-control' ref="zip" placeholder='Zip' />
+						</div>
+						<div className='form-group'>
+							<label className='control-label'>Tell us about your team</label>
+							<input type='text' className='form-control' ref="about" placeholder="About team" />
+						</div>
+						<div className='form-group'>
+							<label className='control-label'>How much are you going to try to raise?</label>
+							<input type='text' className='form-control' ref="amount" placeholder="Enter amount" />
+						</div>
+						<p className='text-center'><button type='submit' className='btn btn-lg btn-success'>Submit</button></p>
+					</form>
+				</div>
 			</div>
 		);
 	}
