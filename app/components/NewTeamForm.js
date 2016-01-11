176632dd1a76fd30f39caff4f4d375676a4fea41
Nick Leonard <nleonard.design@gmail.com>
2016-01-11 09:22:41 -0800

    fixing form issues

--- app/components/NewTeamForm.js
+++ app/components/NewTeamForm.js
@@ -4,8 +4,38 @@ class NewTeamForm extends React.Component {
 	render() {
 
 		return (
-			<div className='alert alert-success text-center'>
-				<p>New Team Form page</p>
+			<div className='container signup'>
+				<div className='alert alert-success text-center'>
+					<p>New Teaam Form page</p>
+				</div>
+
+				<form >						
+					<div className='form-group'>
+						<label className='control-label'>First Name</label>
+						<input type='text' className='form-control' ref="firstname" autoFocus />
+					</div>
+					<div className='form-group'>
+						<label className='control-label'>Last Name</label>
+						<input type='text' className='form-control' ref="lastname" />
+					</div>
+					<div className='form-group'>
+						<label className='control-label'>Username</label>
+						<input type='text' className='form-control' ref="username"/>
+					</div>
+					<div className='form-group'>
+						<label className='control-label'>Email</label>
+						<input type='text' className='form-control' ref="email" />
+					</div>
+					<div className='form-group'>
+						<label className='control-label'>Password</label>
+						<input type='password' className='form-control' ref="password"  />
+					</div>
+					<div className='form-group'>
+						<label className='control-label'>Password Confirmation</label>
+						<input type='password' className='form-control' ref="password2" />
+					</div>
+					<p className='text-center'><button type='submit' className='btn btn-lg btn-success'>Submit</button></p>
+				</form>
 			</div>
 		);
 	}
