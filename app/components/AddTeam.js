ff27221a889e0aff070bf32910184518000c1992
Nick Leonard <nleonard.design@gmail.com>
2016-02-04 14:57:19 -0800

    styled start a team page

--- app/components/AddTeam.js
+++ app/components/AddTeam.js
@@ -7,15 +7,30 @@ class AddTeam extends React.Component {
 	render() {
 		console.log('rendering form page');
 		return (
-			<div className='text-center team-options red-bg'>
-				<h2>Start A Team</h2>
-				<div className='col-sm-6'>
-					<button className='btn btn-large blue-btn'><Link to='/new_team'>Team on a school campus</Link></button>
+		<div className="form_card">
+			<div className='text-left team-options'>
+				<div className="row">
+					<div className='col-sm-12'>
+						<h2>Would you like to start a new team or join a current team?</h2>
+					</div>
 				</div>
-				<div className='col-sm-6'>
-					<button className='btn btn-large blue-btn'><Link to='/new_team'>General Team Stuff</Link></button>
+
+				<div className="row">
+					<div className='col-sm-12'>
+						<div className="banner-border-seperation"></div>
+					</div>
+				</div>
+
+				<div className="row">
+					<div className='col-sm-12'>
+						<button className='btn btn-large red-btn width_100 btn_color'><Link to='/new_team'>Team on a school campus</Link></button>
+					</div>
+					<div className='col-sm-12'>
+						<button className='btn btn-large red-btn width_100 btn_color'><Link to='/new_team'>Join a team</Link></button>
+					</div>
 				</div>
 			</div>
+		</div>
 		);
 	}
 }
