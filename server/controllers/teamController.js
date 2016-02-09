57b7628167a575da7eac0896b8cde09c476e08af
lannng <lannguyen309@gmail.com>
2016-02-08 21:42:56 -0800

    added methods back in teamController

--- server/controllers/teamController.js
+++ server/controllers/teamController.js
@@ -2,6 +2,7 @@ var Model = require('../models/models.js');
 var userController = require('../controllers/userController.js')
 
 let User = Model.User;
+let Team = Model.Team;
 
 /**
  * GET /teams
@@ -9,9 +10,15 @@ let User = Model.User;
  * Returns all teams from Teams table.
  */
 module.exports.getTeams = function(req, res, next) {
-  Model.Team.findAll().then(function(teams) {
-    res.json(teams);
-  })
+  
+  Model.Team.findAll()
+    .then(teams => {
+      res.status(200).json({teams, 'type': 'success', message: 'success'});
+    })
+    .catch(err => {
+      res.status(500).json({ 'type': 'team creation', message: err });
+    })
+
 }
 
 /**
@@ -20,12 +27,15 @@ module.exports.getTeams = function(req, res, next) {
  * Returns team by id, if exists
  */
 module.exports.getTeam = function(req, res, next) {
-  Model.Team.findById(req.params.team_id).then(
-    function(err, team) {
-      if(err) res.send(err);
-        
-    res.json(team);
-  })
+  
+    Model.Team.findById(req.params.team_id)
+    .then(team => {
+      res.status(200).json({team, 'type': 'success', message: 'success'});
+    })
+    .catch(err => {
+      res.status(400).json({ 'type': 'team lookup', message: err });
+    })
+
 }
 
 /**
@@ -83,7 +93,24 @@ module.exports.getTeam = function(req, res, next) {
  * Update specific team based on team_id
  */
 module.exports.putTeam = function(req, res, next) {
-  // TODO
+  
+  let team_name = req.body.team_name;
+  
+  // Fills in blank for any blank fields from form
+  Model.Team.update(
+  {
+    team_name: team_name
+  },
+  {
+    where: { uuid: req.params.team_id }
+  })
+  .then(team => {
+    res.status(201).json({team, 'type': 'success', message: 'successfully updated team'});
+  })
+  .catch(err => {
+    res.status(400).json({ 'type': 'error', message: err });
+  })
+
 }
 
 /**
@@ -93,5 +120,38 @@ module.exports.putTeam = function(req, res, next) {
  * NOTE: This currently only deletes from our local psql DB, NOT from LiNK Salesforce API.
  */
 module.exports.deleteTeam = function(req, res, next) {
-  // TODO
+  Model.Team.destroy(
+    {
+      where: { uuid: req.params.team_id }
+    })
+    .then(uuid => {
+      res.status(201).json({uuid, 'type': 'success', message: 'successfully deleted team from RTP-DB' });
+    })
+    .catch(err => {
+      res.status(400).json({ 'type': 'error', message: err });
+    })
+}
+
+/**
+ * GET /teams/:team_name
+ * 
+ * Returns team by name, if exists
+ */
+ 
+module.exports.getTeamByName = function(req, res, next) {
+
+  Team.find({ where: { team_name: req.params.team_name } })
+      .then(team => {
+        console.log(team.team_name);
+        if (team && team.team_name != null) {
+          res.status(201).json({team, 'type': 'success', message: 'Successfully retrieved team'});
+        } else {
+          res.status(400).json({ 'type': 'error', message: 'No such team!' });
+        }
+        
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json({ 'type': 'error', message: "Team Not Found" });
+      });
 }
