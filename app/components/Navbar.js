62c96fd4b5f016cab02c50bf65fa456ca020ce4a
Nick Leonard <nleonard.design@gmail.com>
2016-02-23 10:22:14 -0800

    got functionality for nav bar, but will have to go back in to get responsive right

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router';
-import {ButtonToolbar, DropdownButton, MenuItem} from 'react-bootstrap';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 import NavbarStore from '../stores/NavbarStore';
 import NavbarActions from '../actions/NavbarActions';
 
-class Navbar extends React.Component {
+class NavComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = NavbarStore.getState();
@@ -33,45 +33,36 @@ class Navbar extends React.Component {
     var os = this;
 
     return (
-      <nav className='navbar navbar-default navbar-static-top'>
-        <div className='navbar-header'>
-          <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar'>
-            <span className='sr-only'>Toggle navigation</span>
-            <span className='icon-bar'></span>
-            <span className='icon-bar'></span>
-            <span className='icon-bar'></span>
-          </button>
-          <div className='navbar-brand'><Link to='/'>
-            <img src="img/link_logo.png" />
-            <h3>Rescue Campaigns</h3>
-          </Link></div>
-        </div>
-        <div id='navbar' className='navbar-collapse collapse'>
-          <ul className='nav navbar-nav'>
-
-            <li><Link to='/login'>Log In</Link></li> 
+      <Navbar inverse>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <div className='navbar-brand'><Link to='/'>
+              <img src="img/link_logo.png" />
+              <h3>Rescue Campaigns</h3>
+            </Link></div>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <li><Link to='/login'>Log In</Link></li>
             <li><Link to='/signup'>Sign Up</Link></li> 
             <li><Link to='/all_teams'>Teams</Link></li> 
             <li><Link to='/all_members'>Members</Link></li>
             <li><Link to='/refugee_stories'>Refugee Stories</Link></li>
-            <li className="dropdown nav-buttons"><ButtonToolbar>
-                <DropdownButton title="Menu" id="dropdown-basic" onSelect={ this.OnSelected } className="just-text dropdown-toggle menu-dropdown btn-group-link">
-                  <li><Link to='/login'>Who & Why we Rescue</Link></li>
-                  <MenuItem divider />
-                  <MenuItem eventKey="4" href="/login">About LiNK</MenuItem>
-                </DropdownButton>
-              </ButtonToolbar>
-            </li>
-           
-          </ul>
-
-          <ul className='nav donate-nav'>
-            <li><Link to='/login'>Donate</Link></li> 
-          </ul>
-        </div>
-      </nav>
+            <NavDropdown title="Menu" id="basic-nav-dropdown">
+              <li><Link to='/login'>Who & Why we Rescue</Link></li>
+              <MenuItem divider />
+              <li><Link to='/login'>About LiNK</Link></li>
+            </NavDropdown>
+          </Nav>
+          <Nav className="nav donate-nav " pullRight>
+            <li className="donate-nav-li"><Link to='/login'>Donate</Link></li> 
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
 
-export default Navbar;
+export default NavComponent;
